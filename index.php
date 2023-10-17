59dada83f6015b06d5b53a23db0f327d740749e3
Estela <eloiseruiz15@gmail.com>
2023-10-17 19:14:50 +0200

    cabecera y sus estilos a medias

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Las siete y media php</title>
+</head>
+<body>
+    <main>
+        <div>
+            <h1 class="titulo">LAS SIETE Y MEDIA</h1>
+
+        <?php
+            if(!isset($_SESSION["baraja"])) {
+                echo '
+                        <h2 class="subtitulo">En php</h2>
+                    </div>
+                    <div>
+
+                    </div>
+                ';
+            }
+        
+        ?>
+    </main>
+</body>
+</html>
